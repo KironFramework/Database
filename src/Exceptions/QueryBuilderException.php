fef060b827faf998c7eb22f5d36cfa9b61ff717b
NielsTRS <contact@lemondedupc.fr>
2018-10-09 18:08:29 +0200

    Ajout des fichiers

---
+++ src/Exceptions/QueryBuilderException.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Created by PhpStorm.
+	 * User: Niels Terese
+	 * Date: 09/10/18
+	 * Time: 18:02
+	 */
+
+	namespace Kiron\Database\Exceptions;
+
+	use \Exception;
+
+	/**
+	 * Class QueryBuilderException
+	 *
+	 * @package Kiron\Database\Exceptions
+	 */
+	class QueryBuilderException extends Exception
+	{
+
+	}
--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -1,8 +1,20 @@
 <?php
 
+	/**
+	 * Created by PhpStorm.
+	 * User: Niels Terese
+	 * Date: 09/10/18
+	 * Time: 18:01
+	 */
+
 	namespace Kiron\Database;
 
 
+	/**
+	 * Class QueryBuilder
+	 *
+	 * @package Kiron\Database
+	 */
 	class QueryBuilder
 	{
 
